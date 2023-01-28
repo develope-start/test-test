1db42349c1feb26410f0a13973dd225a0bdfe3b0
develope-start <develope.start@gmail.com>
2023-01-28 00:33:10 -0800

    learn basic 5 types

---
+++ index-0.js
@@ -0,0 +1,23 @@
+let numb = 69;
+console.log(numb);
+
+console.log(0);
+
+console.log(`chatlaxi`);
+
+let nickname = "chatlax-nastaiashi";
+console.log(nickname);
+
+let masive = [
+  12,
+  `tormeti`,
+  { ierarqia: `wumpe`, ganwyoba: "igjmaji", shefaseba: 5 },
+  true,
+  [10, "oci", 30, false],
+];
+
+console.table(masive);
+
+console.table(masive[2]);
+console.log(masive[2].ganwyoba);
+console.log(masive[4][3]);

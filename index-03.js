bd949fb942e505d3f9e6167917b5a1c17c0ca1c3
develope-start <develope.start@gmail.com>
2023-01-28 02:03:25 -0800

    day 2 learning

---
+++ index-03.js
@@ -0,0 +1,16 @@
+// console.log(2.6);
+
+// console.log(Math.round(2.3));
+
+// let object = {
+//   dila: "tendeba",
+//   game: `ukundeba`,
+//   dro: 1,
+// };
+
+// console.log(Math.floor(2.9));
+// console.log(Math.ceil(2.000000000005));
+
+// console.log(Math.sqrt(2.000000000005));
+// console.log(Math.pow(9, -1));
+console.log(5 + Math.random() * 10);
